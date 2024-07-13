a5134b5c654e696ded2c510034d55c73c357696f
Daksh Patel <dakshpdpu@gmail.com>
2024-07-13 17:17:33 +0530

    Added the logic for cors and nodemon

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import passport from 'passport';
+import cors from 'cors';
 import  {Database}  from './configs/Database';
 import authRoutes from './routes/auth';
 import aiRoutes from './routes/ai';
@@ -14,6 +15,7 @@ dotenv.config();
 const app: Application = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(session({ secret: 'myseessiioonn', resave: false, saveUninitialized: true }));
